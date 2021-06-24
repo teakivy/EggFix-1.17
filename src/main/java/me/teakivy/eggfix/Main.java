b26271bf1ea0923c8ab2d3389f9d2fa0a2416fb6
TeakIvy <53413687+teakivy@users.noreply.github.com>
2021-06-24 01:17:18 -0400

    Update 1.1

--- src/main/java/me/teakivy/eggfix/Main.java
+++ src/main/java/me/teakivy/eggfix/Main.java
@@ -10,12 +10,16 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Objects;
+import java.util.*;
 
 public final class Main extends JavaPlugin implements Listener {
+
+    ArrayList<UUID> mobList = new ArrayList<UUID>();
 
     @Override
     public void onEnable() {
@@ -38,26 +42,42 @@ public final class Main extends JavaPlugin implements Listener {
         }
     }
 
+    @EventHandler
+    public void onDeaths(EntityDeathEvent event) {
+        UUID entityID = event.getEntity().getUniqueId();
+        if (mobList.contains(entityID)) {
+            event.setDroppedExp(0);
+            event.getDrops().clear();
+            mobList.remove(entityID);
+        }
+    }
+
 
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
         if (command.getName().equalsIgnoreCase("eggfix")) {
+            if (!sender.isOp()) {
+                sender.sendMessage(ChatColor.RED + "You need to have OP to run this command!");
+                return true;
+            }
             int count = 0;
             for (Entity entity : Objects.requireNonNull(Bukkit.getServer().getWorld("world")).getEntities()) {
                 if (entity.getType() != EntityType.PLAYER) {
                     if (entity instanceof LivingEntity) {
                         LivingEntity lentity = (LivingEntity) entity;
                         if (Objects.requireNonNull(lentity.getEquipment()).getItemInMainHand().getType() == Material.EGG) {
+                            UUID entityID = lentity.getUniqueId();
+                            mobList.add(entityID);
                             lentity.setHealth(0);
                             count++;
                         }
                     }
-
                 }
             }
             sender.sendMessage("\n" + ChatColor.GRAY + "---------------" + "\n" + ChatColor.YELLOW + "Removed " + ChatColor.GOLD.toString() + ChatColor.BOLD + count + ChatColor.RESET.toString() + ChatColor.YELLOW + " Mobs Holding Eggs!" + "\n" + ChatColor.GRAY + "---------------" + "\n");
+
         }
 
         return false;
